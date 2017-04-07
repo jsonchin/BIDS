f8ec98aba351e7d38a27c8b8da9e092064f4bab5
Jerry Chen <jchen0022@gmail.com>
2017-04-06 23:06:25 -0700

    starting implementation of db profiles

--- flaskr/flaskr/data_management/init_db.py
+++ flaskr/flaskr/data_management/init_db.py
@@ -1,5 +1,16 @@
+from collections import defaultdict
+from IPython.display import display
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+import nltk.stem.wordnet as wordnet
+import numpy as np
 import pandas as pd
-
+import random
+import sklearn
+import string
+import time
 # from flaskr.flaskr.data_management.format_grant_data_sources import *
 
 from format_grant_data_sources import *
@@ -7,7 +18,7 @@ from format_grant_data_sources import *
 import MySQLdb
 
 db =MySQLdb.connect(
-    host='localhost',
+    host='127.0.0.1',
     user='root',
     passwd='pw',
     db='brdo'
@@ -116,6 +127,20 @@ def init_drop_and_create_tables(cur):
                 PRIMARY KEY(grant_title, grant_info_url)
             );""")
 
+    cur.execute("""CREATE TABLE vocabulary (
+                source TEXT,
+                vocab TEXT
+            );""")
+
+    cur.execute("""CREATE TABLE faculty_profiles (
+                name TEXT,
+                profile TEXT
+            );""")
+
+    cur.execute("""CREATE TABLE grants_profiles (
+                name TEXT,
+                profile TEXT
+            );""")
 
 def split_first_last_name(s):
     """
@@ -158,6 +183,105 @@ def init_faculty_vcr(cur):
     sql = """INSERT INTO faculty_vcr VALUES (""" + (" %s," * (len(df.columns.values) - 1)) + """ %s )"""
 
     cur.executemany(sql, rows)
+
+def init_vocab_and_faculty_profiles(cur):
+    """ Preprocessing faculty data
+        Computes TF-IDF over entire faculty corpus """
+    faculty_df = pd.read_csv('temp_data/grants_history.csv', sep="~")
+    descriptions = faculty_df['Title']
+    for desc in descriptions:
+        try:
+            desc = desc.lower()
+            desc = desc.translate
+    faculty_cleaned = []
+    punc_trans = str.maketrans(string.punctuation, " " * len(string.punctuation))
+    num_trans = str.maketrans('', '', '123456789')
+    for desc in descriptions:
+        try:
+            desc = desc.lower()
+            desc = desc.translate(punc_trans)
+            desc = desc.translate(num_trans)
+            first = filter(lambda word: word not in sw, desc.split())
+            second = [wnl.lemmatize(word) for word in first]
+            cleaned.append(second)
+        except:
+            cleaned.append(["Nothing"])
+    faculty_corpus = [' '.join(doc) for doc in faculty_cleaned]
+    faculty_vectorizer = TfidfVectorizer()
+    faculty_matrix = faculty_vectorizer.fit_transform(faculty_corpus)
+    faculty_vocab = dict(faculty_vectorizer.vocabulary_)
+    for word in faculty_vocab:
+        faculty_vocab[word] = int(faculty_vocab[word])
+    faculty_vocab_json = json.dumps(faculty_vocab)
+
+    """ Preprocessing grants data """
+    grants_df = pd.read_csv('temp_data/grants_gov.csv', sep="~")
+    descriptions = grants_df['Description']
+    for desc in descriptions:
+        try:
+            desc = desc.lower()
+            desc = desc.translate
+    grants_cleaned = []
+    for desc in descriptions:
+        try:
+            desc = desc.lower()
+            desc = desc.translate(punc_trans)
+            desc = desc.translate(num_trans)
+            first = = filter(lambda word: word not in sw, desc.split())
+            second = [wnl.lemmatize(word) for word in first]
+            cleaned.append(second)
+        except:
+            cleaned.append(["Nothing"])
+    grants__corpus = [' '.join(doc) for doc in grants_cleaned]
+    grants_vectorizer = TfidfVectorizer(max_df=30000)
+    grants_matrix = grants_vectorizer.fit_transform(grants_corpus)
+    grants_vocab = dict(grants_vectorizer.vocabulary_)
+    for word in grants_vocab:
+        grants_vocab[word] = int(grants_vocab[word])
+    grants_vocab_json = json.dumps(grants_vocab)
+
+    """ Initializing faculty profiles """
+    """
+    # Uses faculty specific vocabulary
+    faculty_profiles = defaultdict(list)
+    for index, row in faculty_df.iterrrows():
+        faculty_profiles[row["PI Name"]].append(faculty_matrix[index])
+    for faculty in faculty_profiles:
+        mean = np.mean(faculty_profiles[faculty])
+        for i in range(len(mean)):
+            mean[i] = float(mean[i])
+        faculty_profiles[faculty] = json.dumps(list(mean))
+    """
+    faculty_profiles = defaultdict(list)
+    for index, row in faculty_df.iterrows():
+        faculty_profiles[row["PI Name"]].append(row["Title"])
+    faculty_vectorizer = TfidfVectorizer(vocabulary=grants_vectorizer.vocabulary_)
+    for faculty in faculty_profiles:
+        research = faculty_profiles[faculty]
+        vectorized = faculty_vectorizer.fit_transform(research)
+        mean = np.mean(vectorized, axis=0)
+        for i in range(len(mean)):
+            mean[i] = float(mean[i])
+        faculty_profiles[faculty] = json.dumps(list(mean))
+
+    """ Initializing grants profiles """
+    grants_profiles = {}
+    for index, row in grant_df.iterrrows():
+        profile = grants_matrix[i]
+        for i in range(len(profile)):
+            profile[i] = float(profile[i])
+        grants_profiles[row["OpportunityTitle"]] = json.dumps(list(profile))
+
+    vocab_stmt = "INSERT INTO vocabulary (source, vocab) VALUES (%s, %s)"
+    vocab_rows = [('faculty', faculty_vocab_json), ('grants', grants_vocab_json)]
+    faculty_stmt = "INSERT INFO faculty_profiles (name, profile) VALUES (%s, %s)"
+    faculty_rows = faculty_profiles.items()
+    grants_stmt = "INSERT INFO grants_profiles (name, profile) VALUES (%s, %s)"
+    grants_rows = grants_profiles.items()
+
+    cur.executemany(vocab_stmt, vocab_rows)
+    cur.executemany(faculty_stmt, faculty_rows)
+    cur.executemany(grants_stmt, grants_rows)
 
 def init_faculty_webpages(cur):
     df = pd.read_csv('temp_data/complete_cleaned_faculty_webpages.csv')
--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,4 +1,162 @@
-from .views import *
-from .HTTP_requests import *
-from .data_management.database import *
 
+#from .views import *
+#from .HTTP_requests import *
+#from .data_management.database import *
+
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+from .utility import *
+
+
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file , flaskr.py
+
+app.debug = True
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'brdo.db'),
+    # SECRET_KEY='development key',
+    USERNAME='root',
+    PASSWORD='pw'
+))
+
+app.config.update(dict(
+    MYSQL_DATABASE_USER='root',
+    MYSQL_DATABASE_PASSWORD='pw',
+    MYSQL_DATABASE_DB='brdo'
+))
+
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+def render_page(html_file_name, css_file):
+    return render_template('page_template.html',
+                           main_content=render_template(html_file_name),
+                           css_file=url_for('static', filename='css/{}'.format(css_file)))
+
+@app.route('/faculty_search_query', methods=['POST'])
+def get_faculty_query():
+    faculty_name = request.form['faculty_name'].lower()
+    qr = get_faculty_vcr(faculty_name)
+    column_names = qr.column_names
+    row = qr.rows[0]
+
+    d = {}
+    for i in range(len(column_names)):
+        if i >= len(row):
+            d[column_names[i]] = ''
+        else:
+            d[column_names[i]] = row[i]
+
+    d['faculty_name'] = captialize_name(d['faculty_name'])
+
+    img_path = url_for('static', filename='images/faculty_images/{}'.format(faculty_name))
+
+    return render_template('response/faculty_query_response.html', faculty_img=img_path, **d) #unpack the dictionary as keyword arguments
+
+@app.route('/manual_grant_submit', methods=['POST'])
+def post_manual_grant():
+    #insert into db
+    print( list(request.form.keys()) )
+    print(request.form['inputGrantTitle'])
+    print(request.form['inputDescription'])
+    print(request.form['inputGrantDeadline'])
+    print(request.form['inputGrantAmt'])
+    print(request.form['inputURLAdditionalInfo'])
+    return ''
+
+@app.route('/manual_faculty_submit', methods=['POST'])
+def post_manual_faculty_submit():
+    """
+    faculty_name,
+    faculty_department
+    faculty_description
+    Lots of blank fields/optional fields
+    :return:
+    """
+    return ''
+
+@app.route('/grant_get_top_k_faculty', methods=['POST'])
+def get_top_k_faculty_for_grant():
+    #get grant data
+    #get faculty
+    #want faculty names to lookup in database
+    #or alternatively all of the info the faculty
+    #render cards for faculty, or pass in data of faculty, list of dictionaries to render in grant_top_k_faculty.html
+    return render_template('response/grant_top_k_faculty.html', faculty_matches=[1,1,1])
+
+def render_page(html_file_name, file_name):
+
+    tabs = [
+        ('Index', '/'),
+        ('Manual Grant Submission', '/manual_grant'),
+        ('Grant Search', '/grant_search'),
+        ('Faculty Search', '/faculty_search')
+    ]
+    tabs_d = []
+    for tab in tabs:
+        tabs_d.append({'name':tab[0], 'route':tab[1]})
+
+
+
+    return render_template('page_template.html',
+                           tabs_d=tabs_d,
+                           main_content=render_template(html_file_name),
+                           css_file=url_for('static', filename='css/{}.css'.format(file_name)),
+                           js_file=url_for('static', filename='js/{}.js'.format(file_name) ) )
+
+
+def render_page_with_html(html, file_name):
+
+    tabs = [
+        ('Index', '/'),
+        ('Manual Grant Submission', '/manual_grant'),
+        ('Grant Search', '/grant_search'),
+        ('Faculty Search', '/faculty_search')
+    ]
+    tabs_d = []
+    for tab in tabs:
+        tabs_d.append({'name':tab[0], 'route':tab[1]})
+
+
+
+    return render_template('page_template.html',
+                           tabs_d=tabs_d,
+                           main_content=html,
+                           css_file=url_for('static', filename='css/{}.css'.format(file_name)),
+                           js_file=url_for('static', filename='js/{}.js'.format(file_name) ) )
+
+@app.route('/')
+def show_index():
+    return render_page('index.html', 'index')
+
+@app.route('/manual_grant')
+def show_manual_grant():
+    return render_page('manual_grant.html', 'manual_grant')
+
+@app.route('/grant_search')
+def show_grant_search():
+    qr = TEMP_get_k_grants()
+    grants = qr.rows
+    column_names = qr.column_names
+    # for grant in grants:
+    #     grant[0] = grant[0].decode('utf8')
+
+    faculty_search_html = render_template('grant_search.html', grants=grants, column_names=column_names)
+
+    return render_page_with_html(faculty_search_html, 'manual_grant')
+
+@app.route('/faculty_search')
+def show_faculty_search():
+    faculty_names = get_faculty_names()
+
+    faculty_names = [captialize_name(name) for name in faculty_names]
+
+    faculty_search_html = render_template('faculty_search.html', faculty_names=faculty_names)
+
+    return render_page_with_html(faculty_search_html, 'faculty_search')
